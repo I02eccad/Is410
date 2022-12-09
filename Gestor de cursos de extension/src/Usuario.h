6ec5497b3f43964443f477ffaff7d6fcb7fab35e
michalspiegel <spiegel.michal@gmail.com>
2022-12-09 20:16:17 +0100

    feat(usuario):anadir roles

--- Gestor de cursos de extension/src/Usuario.h
+++ Gestor de cursos de extension/src/Usuario.h
@@ -8,6 +8,13 @@
 #ifndef USUARIO_H_
 #define USUARIO_H_
 
+enum Rol {
+	Estudiante,
+	CoordinadorDeRecursos,
+	CoordinadorDeCursos,
+	Administrador;
+}
+
 class Usuario {
 public:
 	Usuario(int id, const char *contrasena, const char *nombre, const char *apellidos, const char *dni, const char *email);
@@ -17,7 +24,8 @@ public:
 	char *nombre;
 	char *apellidos;
 	char *dni;
-	char *email;w
+	char *email;
+	Rol rol;
 };
 
 #endif /* USUARIO_H_ */
