473c622372cd57abf071bd09e30c5af777955dbd
michalspiegel <spiegel.michal@gmail.com>
2022-12-16 22:53:22 +0100

    feat(main):crear un demo

--- Gestor de cursos de extension/src/main.cpp
+++ Gestor de cursos de extension/src/main.cpp
@@ -6,9 +6,33 @@
  */
 
 #include "SistemaDeGestionDeCursos.h"
+#include "Curso.h"
 #include <iostream>
+#include <ctime>
 
 int main ()
 {
+	SistemaDeGestionDeCursos sistema = SistemaDeGestionDeCursos();
+	/*
+	vector<const char*> vect1{"camara", "microfono"};
+	vector<int> vect2{12321, 232, 2323, 232};
+	vector<int> vect3{12, 121, 122, 324};
+	vector<int> vect4{123, 1234, 4344};
 
+	Curso calculo("123HD",
+					      "Cálculo",
+			             "Esto es un curso de Cálculo I.",
+						 time(NULL),
+						 vect1,
+						 40, 60,
+						 vect2,
+						 vect3,
+						 123456,
+						 vect4);
+
+
+	sistema.darDeAltaCurso(calculo);
+	*/
+	sistema.verLosCursos();
+	sistema.guardarDatos();
 }
